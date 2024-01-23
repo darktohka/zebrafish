4928d6a768ffb511c95af55654f177bca3bec2d4
Disyer <daniel@tohka.us>
2024-01-23 10:22:24 +0200

    packages: Upgrade nerdctl

--- buildroot-packages/nerdctl/nerdctl.mk
+++ buildroot-packages/nerdctl/nerdctl.mk
@@ -10,7 +10,7 @@ NERDCTL_SITE = $(call github,containerd,nerdctl,main)
 NERDCTL_LICENSE = Apache-2.0
 NERDCTL_LICENSE_FILES = LICENSE
 
-NERDCTL_GOMOD = github.com/containerd/nerdctl
+NERDCTL_GOMOD = github.com/containerd/nerdctl/v2
 
 NERDCTL_BUILD_TARGETS = cmd/nerdctl
 NERDCTL_INSTALL_BINS = nerdctl
