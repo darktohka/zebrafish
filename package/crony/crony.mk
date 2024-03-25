4f654cb5f5b4920f0c8b3d67a7989b6ec205c4b1
Disyer <daniel@tohka.us>
2024-03-25 17:59:16 +0200

    Add crony changes

--- package/crony/crony.mk
+++ package/crony/crony.mk
@@ -5,7 +5,7 @@
 ################################################################################
 
 CRONY_VERSION = main
-CRONY_SITE = $(call github,ClementNerma,Crony,main)
+CRONY_SITE = $(call github,darktohka,Crony,main)
 CRONY_LICENSE = Apache-2.0
 CRONY_LICENSE_FILES = LICENSE.md
 
