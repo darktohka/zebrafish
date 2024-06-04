0f5800dbc2d97c6476b85a48a07e00941f3c2130
Disyer <daniel@tohka.us>
2024-06-04 20:39:25 +0300

    wolfssl: Migrate WolfSSL to CMake

---
+++ buildroot-packages/wolfssl/wolfssl.mk
@@ -0,0 +1,17 @@
+################################################################################
+#
+# wolfssl
+#
+################################################################################
+
+WOLFSSL_VERSION = master
+WOLFSSL_SITE = $(call github,wolfSSL,wolfssl,master)
+WOLFSSL_INSTALL_STAGING = YES
+
+WOLFSSL_LICENSE = GPL-2.0+
+WOLFSSL_LICENSE_FILES = COPYING LICENSING
+WOLFSSL_DEPENDENCIES = host-pkgconf
+
+WOLFSSL_CONF_OPTS = -DWOLFSSL_ASM=yes -DWOLFSSL_EXAMPLES=no -DWOLFSSL_CRYPT_TESTS=no -DWOLFSSL_OLD_TLS=no
+
+$(eval $(cmake-package))
