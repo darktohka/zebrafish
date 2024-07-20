e7f88368e6bfdd1fa7bad7cb8379b3c9f7ece046
Disyer <daniel@tohka.us>
2024-07-20 13:10:45 +0300

    Fix curl, nghttp3 Add elfutils because of error Disable ugrep for now Change dropbear back to upstream

--- buildroot-packages/nerdctl/nerdctl.mk
+++ buildroot-packages/nerdctl/nerdctl.mk
@@ -5,7 +5,7 @@
 ################################################################################
 
 NERDCTL_VERSION = main
-NERDCTL_SITE = $(call github,containerd,nerdctl,main)
+NERDCTL_SITE = $(call github,darktohka,nerdctl,main)
 
 NERDCTL_LICENSE = Apache-2.0
 NERDCTL_LICENSE_FILES = LICENSE
@@ -14,5 +14,11 @@ NERDCTL_GOMOD = github.com/containerd/nerdctl/v2
 
 NERDCTL_BUILD_TARGETS = cmd/nerdctl
 NERDCTL_INSTALL_BINS = nerdctl
+
+define NERDCTL_REMOVE_GO_SUM
+	rm -f $(@D)/go.sum
+endef
+
+NERDCTL_POST_EXTRACT_HOOKS += NERDCTL_REMOVE_GO_SUM
 
 $(eval $(golang-package))
