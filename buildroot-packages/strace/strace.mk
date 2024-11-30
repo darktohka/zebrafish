10ebec4a94052c9c8ed3fbecb4830f9e50e6754d
Disyer <daniel@tohka.us>
2024-11-30 23:58:04 +0200

    Add strace

---
+++ buildroot-packages/strace/strace.mk
@@ -0,0 +1,34 @@
+################################################################################
+#
+# strace
+#
+################################################################################
+
+STRACE_VERSION = master
+STRACE_SITE = $(call github,strace,strace,master)
+STRACE_CONF_OPTS = --enable-mpers=no
+
+ifeq ($(BR2_PACKAGE_LIBUNWIND),y)
+STRACE_DEPENDENCIES += libunwind
+STRACE_CONF_OPTS += --with-libunwind
+else
+STRACE_CONF_OPTS += --without-libunwind
+endif
+
+# Demangling symbols in stack trace needs libunwind and libiberty.
+ifeq ($(BR2_PACKAGE_BINUTILS)$(BR2_PACKAGE_LIBUNWIND),yy)
+STRACE_DEPENDENCIES += binutils
+STRACE_CONF_OPTS += --with-libiberty=check
+else
+STRACE_CONF_OPTS += --without-libiberty
+endif
+
+ifeq ($(BR2_PACKAGE_PERL),)
+define STRACE_REMOVE_STRACE_GRAPH
+	rm -f $(TARGET_DIR)/usr/bin/strace-graph
+endef
+
+STRACE_POST_INSTALL_TARGET_HOOKS += STRACE_REMOVE_STRACE_GRAPH
+endif
+
+$(eval $(autotools-package))
