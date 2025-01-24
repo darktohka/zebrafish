08c1447279220d99450928228e68d955e4a187ac
Disyer <daniel@tohka.us>
2025-01-24 23:47:33 +0200

    Add new dive binary

--- package/dive/dive.mk
+++ package/dive/dive.mk
@@ -5,10 +5,17 @@
 ################################################################################
 
 DIVE_VERSION = main
-DIVE_SITE = $(call github,wagoodman,dive,main)
+DIVE_SITE = $(call github,darktohka,dive,zebrafish)
 DIVE_LICENSE = MIT
 DIVE_LICENSE_FILES = LICENSE
 
-DIVE_GOMOD = github.com/wagoodman/dive
+DIVE_GOMOD = github.com/darktohka/dive
+
+define DIVE_REWRITE_SCRIPT
+	$(SED) 's/"container-engine", "docker"/"container-engine", "nerdctl"/' $(@D)/cmd/root.go
+	$(SED) 's/"source", "docker"/"source", "nerdctl"/' $(@D)/cmd/root.go
+endef
+
+DIVE_POST_PATCH_HOOKS += DIVE_REWRITE_SCRIPT
 
 $(eval $(golang-package))
