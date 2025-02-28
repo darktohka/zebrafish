28a555080e2ff452a18c7fd06acc9d3bad28ddf7
Disyer <daniel@tohka.us>
2025-02-28 15:17:09 +0200

    Fix knock-rs link

--- package/knockrs/knockrs.mk
+++ package/knockrs/knockrs.mk
@@ -5,7 +5,7 @@
 ################################################################################
 
 KNOCKRS_VERSION = master
-KNOCKRS_SITE = $(call github,darktohka,knockrs,master)
+KNOCKRS_SITE = $(call github,darktohka,knock-rs,master)
 KNOCKRS_LICENSE = Apache-2.0
 KNOCKRS_LICENSE_FILES = LICENSE
 
