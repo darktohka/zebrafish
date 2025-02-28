83fa329f514bd3ee4d69bfddcfb36384f9bc1eaa
Disyer <daniel@tohka.us>
2025-02-28 14:45:53 +0200

    Add knockrs

---
+++ package/knockrs/knockrs.mk
@@ -0,0 +1,12 @@
+################################################################################
+#
+# knockrs
+#
+################################################################################
+
+KNOCKRS_VERSION = master
+KNOCKRS_SITE = $(call github,darktohka,knockrs,master)
+KNOCKRS_LICENSE = Apache-2.0
+KNOCKRS_LICENSE_FILES = LICENSE
+
+$(eval $(cargo-package))
