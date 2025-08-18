d6ea8dbf54e647de2d55640f9ca7b12df99bca47
Disyer <daniel@tohka.us>
2025-08-19 00:55:15 +0300

    Remove vendoring from docker-cli: experimental

--- buildroot-packages/docker-cli/docker-cli.mk
+++ buildroot-packages/docker-cli/docker-cli.mk
@@ -23,15 +23,6 @@ DOCKER_CLI_TAGS += osusergo netgo
 DOCKER_CLI_GO_ENV = CGO_ENABLED=no
 endif
 
-# create the go.mod file with language version go1.25
-# remove the conflicting vendor/modules.txt
-# https://github.com/moby/moby/issues/44618#issuecomment-1343565705
-define DOCKER_CLI_FIX_VENDORING
-	printf "module $(DOCKER_CLI_GOMOD)\n\ngo 1.25\n" > $(@D)/go.mod
-	rm -f $(@D)/vendor/modules.txt
-endef
-DOCKER_CLI_POST_EXTRACT_HOOKS += DOCKER_CLI_FIX_VENDORING
-
 DOCKER_CLI_INSTALL_BINS = $(notdir $(DOCKER_CLI_BUILD_TARGETS))
 
 $(eval $(golang-package))
