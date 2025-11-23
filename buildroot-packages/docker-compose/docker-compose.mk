c919fc857a69885c73c643b621753179101209bd
Disyer <daniel@tohka.us>
2025-11-23 18:58:20 +0200

    Update Docker-compose to version 5

--- buildroot-packages/docker-compose/docker-compose.mk
+++ buildroot-packages/docker-compose/docker-compose.mk
@@ -10,9 +10,9 @@ DOCKER_COMPOSE_LICENSE = Apache-2.0
 DOCKER_COMPOSE_LICENSE_FILES = LICENSE
 
 DOCKER_COMPOSE_BUILD_TARGETS = cmd
-DOCKER_COMPOSE_GOMOD = github.com/docker/compose/v2
+DOCKER_COMPOSE_GOMOD = github.com/docker/compose/v5
 DOCKER_COMPOSE_LDFLAGS = \
-	-X github.com/docker/compose/v2/internal.Version=$(DOCKER_COMPOSE_VERSION)
+	-X github.com/docker/compose/v5/internal.Version=$(DOCKER_COMPOSE_VERSION)
 
 define DOCKER_COMPOSE_INSTALL_TARGET_CMDS
 	$(INSTALL) -D -m 755 $(@D)/bin/cmd \
