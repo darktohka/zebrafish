6d87a6ad933c967b550c8ceb06b03392260e068c
Disyer <daniel@tohka.us>
2026-04-19 13:20:36 +0300

    Update Linux to 7.0 and buildroot

--- buildroot-packages/strace/strace.mk
+++ buildroot-packages/strace/strace.mk
@@ -4,9 +4,8 @@
 #
 ################################################################################
 
-STRACE_VERSION = 6.19
-STRACE_SOURCE = strace-$(STRACE_VERSION).tar.xz
-STRACE_SITE = https://github.com/strace/strace/releases/download/v$(STRACE_VERSION)
+STRACE_VERSION = master
+STRACE_SITE = $(call github,strace,strace,master)
 STRACE_LICENSE = LGPL-2.1+
 STRACE_LICENSE_FILES = COPYING LGPL-2.1-or-later
 STRACE_CPE_ID_VALID = YES
